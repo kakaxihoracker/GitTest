ac607dc9098c00dc63245dea08ad05d22f349832
horacker <xuzhengguang@huoqiu.cn>
2017-05-27 15:25:29 +0800

    系统调试悬浮工具

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -19,6 +19,18 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"first git test");
     
+    UILabel *lab = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 100, 40)];
+    lab.backgroundColor = [UIColor purpleColor];
+    [self.view addSubview:lab];
+
+    // https://juejin.im/post/59291f1c44d9040064213c0f
+    // http://ryanipete.com/blog/ios/swift/objective-c/uidebugginginformationoverlay/?from=timeline&isappinstalled=0
+    // 系统再带悬浮窗口调试工具
+   UIWindow *debug =  (UIWindow *)NSClassFromString(@"UIDebuggingInformationOverlay");
+    [debug performSelector:NSSelectorFromString(@"prepareDebuggingOverlay")];
+   SEL debugSel = NSSelectorFromString(@"overlay");
+    [[debug performSelector:debugSel] performSelector:NSSelectorFromString(@"toggleVisibility")];
+    
 }
 
 
